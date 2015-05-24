962363a708df3befef0166159d2a0c05b8d838a6
Shalisska <alena.simonova83@gmail.com>
2015-05-24 22:28:48 +0300

    2405

---
+++ Gruntfile.js
@@ -0,0 +1,199 @@
+module.exports = function (grunt) {
+	
+	require('load-grunt-tasks')(grunt);
+	
+	grunt.initConfig({
+		//компиляция less
+		less: {
+			style: {
+				files: {
+					'app/css/style.css' : ['app/less/common.less']
+				}
+			}
+		},
+		//автопрефиксер
+		autoprefixer: {
+			options: {
+				browsers: ['last 2 versions', 'ie 8']
+			},
+			style: {
+				src: 'app/css/style.css'
+			}
+		},
+		//комбинирует медиа-выражения
+		cmq: {
+			style: {
+				files: {
+					'app/css/style.css' : ['app/css/style.css']
+				}
+			}
+		},
+		//минимизирует стилевой файл
+		cssmin: {
+			style: {
+				options: {
+					KeepSpecialComments: 0,
+					report: 'gzip'
+				},
+				files: {
+					'build/css/style.min.css' : ['build/css/style.css']
+				}
+			}
+		},
+		//делает стилевой файл красивым
+		csscomb: {
+			style: {
+				files: {
+					'app/css/style.css' : ['app/css/style.css']
+				}
+			}
+		},
+		//минимизирует изображения
+		imagemin: {
+			build: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [{
+					expand: true,
+					src: ['app/images/**/*.{png, jpg, gif, svg}']
+				}]
+			}
+		},
+		//минимизирует html-файлы
+		htmlmin: {
+			options: {
+				removeComments: true,
+				collapseWhitespace: true,
+				collapseBooleanAttributes: true,
+				caseSensitive: true,
+				keepClosingSlash: true
+			},
+			build: {
+				files: {
+					'build/index.html' : 'build/index.html'
+				}
+			}
+		},
+		//очистка папки проекта
+		clean: {
+			build: ['build']
+		},
+		//копирование файлов в папку проекта
+		copy: {
+			build: {
+				files: [{
+					expand: true,
+					src: [
+						'app/css/**',
+						'app/images/**',
+						'app/scripts/**'
+					],
+					dest: 'build'
+				}]
+			},
+			//копирование только нужных html-файлов
+			build_html: {
+				files: [{
+					expand: true,
+					cwd: 'html/',
+					src:['*.html'],
+					dest: 'build'
+				}]
+			}
+		},
+		//работа с jade-файлами
+		jade: {
+			//основные файлы
+			temp: {
+				options: {
+					pretty: true
+				},
+				files: [{
+					expand: true,
+					cwd: 'app/jade/pages/',
+					src: ['*.jade'],
+					dest: 'app/html/',
+					ext: '.html',
+					extDot: 'last'
+				}]
+			}
+		},
+		//замена пробелов табами в html-файлах
+		prettify: {
+			options: {
+				indent: 1,
+				indent_char: '	'
+			},
+			html: {
+				expand: true,
+				cwd: 'app/html',
+				ext: '.html',
+				src: '**/*.html',
+				dest: 'app/html'
+			}
+		},
+		//запуск автоматических функций
+		watch: {
+			//компилятор less и автопрефиксер
+			styles: {
+				files: ['app/less/**/*.less'],
+				tasks: ['less', 'autoprefixer'],
+				options: {
+					spawn: false
+				}
+			},
+			//компилятор jade
+			jades: {
+				files: ['app/jade/**/*.jade'],
+				tasks: ['jade'],
+				options: {
+					spawn: false
+				}
+			}
+		},
+		//минимизатор JS
+		uglify: {
+			build: {
+				src: 'build/js/js.js',
+				dest: 'build/js/js.min.js'
+			}
+		},
+		//замена относительных адресов
+		replace: {
+			build: {
+				options: {
+					patterns: [{
+						match: /src="..\//g,
+						replacement: 'src="'
+					}, {
+						match: /href="..\//g,
+						replacement: 'href="'
+					}]
+				},
+				files: [{
+					expand: true,
+					flattern: true,
+					src: ['build/*.html']
+				}]
+			}
+		}
+	});
+	
+	grunt.registerTask('default', [
+//		'imagemin',
+		'clean',
+		'copy',
+//		'cssmin',
+//		'htmlmin',
+//		'uglify',
+		'replace'
+	]);
+	
+	grunt.registerTask('debug', [
+		'less',
+		'autoprefixer',
+		'cmq',
+		'csscomb'
+	]);
+};
